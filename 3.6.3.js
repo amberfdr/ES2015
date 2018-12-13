924070c961b49a0f3af3d219353c332ce8bab09c
amberfdr <amberfdr@163.com>
2018-12-13 23:41:34 +0800

    解构参数

---
+++ 3.6.3.js
@@ -0,0 +1,11 @@
+let that = this;
+function fn(){
+    console.log(this);
+    console.log(that === this);
+    // console.log(arguments);
+}
+var obj = {foo:'bar'}
+fn();
+// fn.apply(obj,[1,2,3]);
+
+// fn.call(obj,1,2,3);
